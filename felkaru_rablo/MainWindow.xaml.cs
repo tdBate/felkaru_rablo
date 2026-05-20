53ccc30e6c678e970789c4d06b0bce80ffdd7326
TorokD <td.zazsa@gmail.com>
2026-05-20 08:38:01 +0200

    Alapveto funkciok kész

--- felkaru_rablo/MainWindow.xaml.cs
+++ felkaru_rablo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,41 @@ namespace felkaru_rablo
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random rnd = new Random();
+
+        string[] szimbulomok = {"Cseresznye", "Banán", "Barack", "Dinnye"};
+        int cellakSzama = 3;
         public MainWindow()
         {
             InitializeComponent();
+            Porget();
+        }
+
+        public string RandomGyumolcs() 
+        {
+            return szimbulomok[rnd.Next(0, szimbulomok.Length)];
+        }
+
+        public void Porget()
+        {
+            string[] eredmenyek = new string[cellakSzama];
+            for (int i = 0; i < cellakSzama; i++)
+            {
+                eredmenyek[i] = RandomGyumolcs();
+            }
+            Ellenoriz(eredmenyek);
+            Debug.WriteLine(string.Join(";", eredmenyek) + " - " + Ellenoriz(eredmenyek));
+        }
+
+        public int Ellenoriz(string[] eredmenyek)
+        {
+            foreach (string element in eredmenyek)
+            {
+                int count = eredmenyek.Count(item=> element==item);
+                if (count == 2) { return 20; }
+                else if (count == 3) { return 50; }
+            }
+            return 0;
         }
     }
 }
